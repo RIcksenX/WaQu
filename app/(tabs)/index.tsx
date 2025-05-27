365c02b903397a4ee8176380281c0958728a857c
RicksenX <evan.hartono@binus.ac.id>
2025-05-27 16:11:28 +0700

    install react native 1.18.0, add map to index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,105 @@
-import { Link } from "expo-router";
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useRef, useState } from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 
-export default function Index() {
+const INITIAL_REGION = {
+    latitude: -6,
+    longitude: 106,
+    latitudeDelta: 2,
+    longitudeDelta: 2
+}
+
+export default function App() {
+
+  // const [marker, setMarker] = useState({});
+  //   let markerRef = useRef(null);
+
+  //   useEffect(() => {
+  //       if (markerRef.current) {
+  //           markerRef.current.showCallout();
+  //       }
+  //   })
+    
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-light-200 font-bold">hello test</Text>
+    <View style={{ flex: 1 }}>
+    <MapView style={StyleSheet.absoluteFill} 
+    provider={PROVIDER_GOOGLE} 
+    initialRegion={INITIAL_REGION}
+    region={{ 
+      latitude: -6,
+      longitude: 106,
+      latitudeDelta: 2,
+      longitudeDelta: 2,
+      name: 'asdf'
+    }}
+    // showsUserLocation
+    // showsMyLocationButton
+    >
+      <Marker 
+        coordinate={INITIAL_REGION}
+        title='jakarta'
+        description='test'
+        // image={require('../assets/images/adaptive-icon.png')}
+      >
+
+        {/* <Callout tooltip={true}>
+          <View>
+              <Text style={{ fontSize:24 }}>test</Text>
+              <Text> asdf </Text>
+          </View>
+        </Callout> */}
+
+      </Marker>
+    </MapView>
     </View>
   );
 }
+
+// export default ExploreScreen;
+
+const styles = StyleSheet.create({
+  bubble: {
+    flexDirection: 'row',
+    alignSelf: 'flex-start',
+    backgroundColor: '#fff',
+    borderRadius: 6,
+    borderColor: '#ccc',
+    borderWidth: 0.5,
+    padding: 15,
+    width: 150
+  },
+  name: {
+    fontSize: 16,
+    marginBottom: 5
+  },
+  image: {
+    width: 120,
+    height: 80
+  },
+  arrow: {
+    backgroundColor: 'transparent',
+    borderColor: 'transparent',
+    borderTopColor: '#fff',
+    borderWidth: 16,
+    alignSelf: 'center',
+    marginTop: -32
+  },
+  arrowBorder: {
+    backgroundColor: 'transparent',
+    borderColor: 'transparent',
+    borderTopColor: '#007a87',
+    borderWidth: 16,
+    alignSelf: 'center',
+    marginTop: -0.5
+  }
+})
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+//   map: {
+//     width: '100%',
+//     height: '100%',
+//   },
+// });
